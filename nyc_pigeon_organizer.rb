e7ee17c9a1b9dc23173844c31b8ec9f7443f4641
jameslee1187 <jameslee1187@gmail.com>
2018-10-04 21:45:31 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,26 @@
 def nyc_pigeon_organizer(data)
-new_hash = {}
-data.each do |key, value|
-  value.each do |type, spec|
-    spec.each do |names|
-      if !new_hash.has_key?(names)
-        new_hash[names] = {}
-      end
-        if !new_hash[names].has_key?(key)
-          new_hash[names][key] = []
-        end
-          if !new_hash[names][key].include?(type)
-            new_hash[names][key] << type.to_s
-          end
-      end
-    end
-  end
+# new_hash = {}
+# data.each do |key, value|
+#   value.each do |type, spec|
+#     spec.each do |names|
+#       if !new_hash.has_key?(names)
+#         new_hash[names] = {}
+#       end
+#         if !new_hash[names].has_key?(key)
+#           new_hash[names][key] = []
+#         end
+#           if !new_hash[names][key].include?(type)
+#             new_hash[names][key] << type.to_s
+#           end
+#       end
+#     end
+#   end
 
-p new_hash
-new_hash
+# p new_hash
+# new_hash
         
-         
+new_hash = {}
+
 end
+
+
