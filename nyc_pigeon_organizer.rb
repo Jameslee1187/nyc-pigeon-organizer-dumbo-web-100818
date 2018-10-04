cdcc1ea55b1ff08f3c75d558e193e94dfaa1f250
jameslee1187 <jameslee1187@gmail.com>
2018-10-04 22:16:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,9 +23,13 @@ new_hash = {}
 data[:color].each |key, value|
 value.each do |name|
   if new_hash.has_key?(name)
-  new_hash[name] = {}
-  new_hash[name][:color] =  [key]
+    new_hash[name][:color] << key
+  else
+    new_hash[name] = {}
+    new_hash[name][:color] =  [key]
    #key is purple value is names
+   #if key is not newHash = { "Theo" => {:color=>["purple"]}}
+  end
 end
 
 
