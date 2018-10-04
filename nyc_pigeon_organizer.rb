8f3442dd0eb16879434dfebad9b9a61434075ff1
jameslee1187 <jameslee1187@gmail.com>
2018-10-04 20:56:33 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
 new_hash = {}
 data.each do |key, value|
-  value.each do |type, color|
-    color.each do |
+  value.each do |type, spec|
+    spec.each do |names|
+      
 end
